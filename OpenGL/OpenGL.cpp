b56e69524b759815d26847c6df4352731bc3ff3b
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-09-20 23:24:04 -0400

    Checked the vector library

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -1,9 +1,22 @@
 #include "GameController.h"
 
+static void AddVectors()
+{
+	glm::vec2 a = { 3, 5 };
+	glm::vec2 b = { 2, -1 };
+	glm::vec2 c = a + b;
+	std::cout << glm::to_string(c) << std::endl;
+
+	glm::vec3 d = { 3, 5, 1 };
+	glm::vec3 e = { 2, -1, -1 };
+	glm::vec3 f = d + e;
+	std::cout << glm::to_string(f) << std::endl;
+}
+
 int main(void)
 {
-    GameController::GetInstance().Initialize();
-    GameController::GetInstance().RunGame();
-
+    // GameController::GetInstance().Initialize();
+    // GameController::GetInstance().RunGame();
+	AddVectors();
     return 0;
 }
--- OpenGL/StandardIncludes.h
+++ OpenGL/StandardIncludes.h
@@ -1,10 +1,13 @@
 #ifndef STANDARD_INCLUDES_H
 #define STANDARD_INCLUDES_H
 
+#define GLM_ENABLE_EXPERIMENTAL
+
 // Includes standard headers
 #include <vector>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 // Windows specific includes and defines
 #ifdef WIN32
@@ -17,7 +20,7 @@
 #include <GL/glew.h>     // Include GLEW
 #include <GLFW/glfw3.h>  // Include GLFW
 #include <glm/glm.hpp>   // Include GLM
-
+#include <glm/ext.hpp>   // Open Experimental to_string
 #include "Singleton.h"
 
 using namespace std;
