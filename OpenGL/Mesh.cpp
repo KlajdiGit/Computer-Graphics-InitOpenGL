be899f6a354124dbdddcbd48d7c73fc401e51fe8
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-09-29 14:23:37 -0400

    Did the implementation of the previous parts

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -19,28 +19,39 @@ void Mesh::Create(Shader* _shader)
 	float a = 26.0f;
 	float b = 42.0f;
 
-
-	m_vertexData = { 
-		/*  Position  */  /*   RGBA Color     */
-		-a, 0.0f, b,  1.0f, 0.0f, 0.0f, 1.0f, 
-		a, 0.0f, b,   1.0f, 0.549f, 0.0f, 1.0f,
-		-a, 0.0f, -b, 1.0f, 1.0f, 0.0f, 1.0f,
-		a, 0.0f, -b,  1.0f, 1.0f, 0.0f, 1.0f,
-		0.0f, b, a,   0.0f, 0.0f, 0.0f, 1.0f,
-		0.0f, b, -a,  0.294f, 0.0f, 0.51f, 1.0f,
-		0.0f, -b, a,  0.502f, 0.0f, 0.502f, 1.0f,
-		0.0f, -b, -a, 1.0f, 1.0f, 1.0f, 1.0f,
-		b, a, 0.0f,   0.0f, 1.0f, 1.0f, 1.0f,
-		-b, a, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,
-		b, -a, 0.0f,  0.118f, 0.565, 1.0f, 1.0f,
-		-b, -a, 0.0f, 0.863f, 0.078f, 0.235f, 1.0f
-
+	m_vertexData = {
+		/* Position   */  /*    RGBA Color    */
+        0.2f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+        0.3f, 0.9f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+        0.4f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.7f, 0.8f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+        0.8f, 0.4f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 0.6f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        1.0f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+        1.5f, 0.6f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f
 	};
+
+	//m_vertexData = { 
+	//	/*  Position  */  /*   RGBA Color     */
+	//	-a, 0.0f, b,  1.0f, 0.0f, 0.0f, 1.0f, 
+	//	a, 0.0f, b,   1.0f, 0.549f, 0.0f, 1.0f,
+	//	-a, 0.0f, -b, 1.0f, 1.0f, 0.0f, 1.0f,
+	//	a, 0.0f, -b,  1.0f, 1.0f, 0.0f, 1.0f,
+	//	0.0f, b, a,   0.0f, 0.0f, 0.0f, 1.0f,
+	//	0.0f, b, -a,  0.294f, 0.0f, 0.51f, 1.0f,
+	//	0.0f, -b, a,  0.502f, 0.0f, 0.502f, 1.0f,
+	//	0.0f, -b, -a, 1.0f, 1.0f, 1.0f, 1.0f,
+	//	b, a, 0.0f,   0.0f, 1.0f, 1.0f, 1.0f,
+	//	-b, a, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f,
+	//	b, -a, 0.0f,  0.118f, 0.565, 1.0f, 1.0f,
+	//	-b, -a, 0.0f, 0.863f, 0.078f, 0.235f, 1.0f
+
+	//};
 	glGenBuffers(1, &m_vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, m_vertexData.size() * sizeof(float), m_vertexData.data(), GL_STATIC_DRAW);
 
-	m_indexData = {
+	/*m_indexData = {
 		0, 6, 1, 0, 11, 6, 1, 4, 0, 1, 8, 4,
 		1, 10, 8, 2, 5, 3, 2, 9, 5, 2, 11, 9,
 		3, 7, 2, 3, 10, 7, 4, 8, 5, 4, 9, 0,
@@ -49,7 +60,7 @@ void Mesh::Create(Shader* _shader)
 	};
 	glGenBuffers(1, &m_indexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_indexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, m_indexData.size() * sizeof(GLubyte), m_indexData.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_indexData.size() * sizeof(GLubyte), m_indexData.data(), GL_STATIC_DRAW);*/
 
 }
 
@@ -83,14 +94,15 @@ void Mesh::Render(glm::mat4 _wvp)
 		(void*) (3 * sizeof(float)));           // array buffer offset 
 	
 	//3rd attribute: WVP
-	m_world = glm::rotate(m_world, 0.001f, { 0, 1, 0 });
+	//m_world = glm::rotate(m_world, 0.001f, { 0, 1, 0 });
 	_wvp *= m_world;
 	glUniformMatrix4fv(m_shader->GetAttrWVP(), 1, GL_FALSE, &_wvp[0][0]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer); // Bind the vertex buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer); // Bind the index buffer
-	// GL_TRIANGLES, GL_LINES, GL_LINE_STRIP
-	glDrawElements(GL_TRIANGLES, m_indexData.size(), GL_UNSIGNED_BYTE, (void*)0); // Draw the triangles
+	//glDrawElements(GL_TRIANGLES, m_indexData.size(), GL_UNSIGNED_BYTE, (void*)0); // Draw the triangles
+	// GL_TRIANGLE_STRIP, GL_LINES, GL_LINE_STRIP
+	glDrawArrays(GL_LINE_STRIP, 0, m_vertexData.size() / 7);
 	glDisableVertexAttribArray(m_shader->GetAttrVertices());
 	glDisableVertexAttribArray(m_shader->GetAttrColors());
 
