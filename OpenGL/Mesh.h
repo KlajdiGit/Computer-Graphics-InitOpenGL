403f64c56e4f99468cd3feb5cbe92d81ced60f38
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-09-28 23:28:00 -0400

    Finished the slide code

--- OpenGL/Mesh.h
+++ OpenGL/Mesh.h
@@ -20,7 +20,9 @@ public:
 private:
 	Shader* m_shader;
 	GLuint m_vertexBuffer;
-	vector<GLfloat> m_vertexData;
+	GLuint m_indexBuffer; // GPU buffer
+	vector<GLfloat> m_vertexData; // Store vertex data in RAM
+	std::vector<GLubyte> m_indexData; // Store index data in RAM
 	glm::mat4 m_world;
 };
 
