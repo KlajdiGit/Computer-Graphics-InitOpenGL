5a35c974761739ad681e3e6be15bb0d538779dea
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-10-14 20:59:31 -0400

    test

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -91,7 +91,7 @@ void Mesh::Render(glm::mat4 _wvp)
 	//4th attribute: WVP
 	m_rotation.y += 0.005f;
 	glm::mat4 transform = glm::rotate(_wvp, m_rotation.y, glm::vec3(0, 1, 0));
-	glUniformMatrix4fv(m_shader->GetAttrWVP(), 1, GL_FALSE, &transform[0][0]);
+    glUniformMatrix4fv(m_shader->GetAttrWVP(), 1, GL_FALSE, &transform[0][0]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer); // Bind the vertex buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer); // Bind the index buffer
