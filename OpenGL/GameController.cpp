6d781ef433890e7ad38f67fb9255bd765bac5f25
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-11-15 22:18:40 -0500

    Output 6 completed

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -21,6 +21,13 @@ void GameController::Initialize()
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
+	// glEnable(GL_CULL_FACE);
+	// glCullFace(GL_FRONT);
 	srand((unsigned int)time(0));
 
 	//Create a default perspective camera
@@ -45,12 +52,12 @@ void GameController::RunGame()
 	m.SetScale({ 0.01f, 0.01f, 0.01f });
 	Mesh::Lights.push_back(m);
 
-	Mesh teapot = Mesh();
+	/*Mesh teapot = Mesh();
 	teapot.Create(&m_shaderDiffuse, "../Assets/Models/teapot.obj");
 	teapot.SetCameraPosition(m_camera.GetPosition());
 	teapot.SetScale({ 0.02f, 0.02f, 0.02f });
 	teapot.SetPosition({ 0.0f, 0.0f, 0.0f });
-	m_meshes.push_back(teapot);
+	m_meshes.push_back(teapot);*/
 
 	Mesh box = Mesh();
 	box.Create(&m_shaderDiffuse, "../Assets/Models/Cube.obj");
@@ -59,19 +66,19 @@ void GameController::RunGame()
 	box.SetPosition({ -1.0f, -1.0f, -1.0f });
 	m_meshes.push_back(box);
 
-	Mesh plane = Mesh();
+	/*Mesh plane = Mesh();
 	plane.Create(&m_shaderDiffuse, "../Assets/Models/Plane.obj");
 	plane.SetCameraPosition(m_camera.GetPosition());
 	plane.SetScale({ 0.3f, 0.3f, 0.3f });
 	plane.SetPosition({ 0.0f, 0.0f, 0.0f });
-	m_meshes.push_back(plane);
+	m_meshes.push_back(plane);*/
 
-	Mesh window = Mesh();
+	/*Mesh window = Mesh();
 	window.Create(&m_shaderDiffuse, "../Assets/Models/Window.obj");
 	window.SetCameraPosition(m_camera.GetPosition());
 	window.SetScale({ 0.1f, 0.1f, 0.1f });
 	window.SetPosition({ 0.0f, 0.0f, 0.0f });
-	m_meshes.push_back(window);
+	m_meshes.push_back(window);*/
 
 	Fonts f = Fonts();
 	f.Create(&m_shaderFont, "arial.ttf", 100);
