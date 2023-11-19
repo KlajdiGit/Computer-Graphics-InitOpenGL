aa06197e959f9da3cabe1f6cfba02cfdace3d2dd
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2023-11-19 12:22:33 -0500

    Test

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -22,12 +22,12 @@ void GameController::Initialize()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	
-	glEnable(GL_CULL_FACE);
+	/*glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
-	glFrontFace(GL_CW);
+	glFrontFace(GL_CW);*/
 
-	// glEnable(GL_CULL_FACE);
-	// glCullFace(GL_FRONT);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_FRONT);
 	srand((unsigned int)time(0));
 
 	//Create a default perspective camera
